01efb5cd4e8a4037c28fb41f4687c37a7915c371
Cromwel Penaranda <c.m.penaranda@hotmail.com>
2018-03-26 19:04:17 +0800

    3rd commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { ModuleWithProviders } from '@angular/core'
 import { Routes, RouterModule } from '@angular/router'
 
 import { AppComponent, } from './app.component'
+import { AboutComponent } from './about/about.component'
 import { RidelibraryComponent } from './ridelibrary/ridelibrary.component'
 import { RidecalendarComponent } from './ridecalendar/ridecalendar.component'
 import { AppHomeComponent } from './apphome/app-home.component'
@@ -10,7 +11,8 @@ export const router: Routes = [
 {path: '', redirectTo: 'apphome', pathMatch: 'full'},
 {path: 'apphome', component: AppHomeComponent},
 {path: 'ridecalendar', component: RidecalendarComponent},
-{path: 'ridelibrary', component: RidelibraryComponent}
+{path: 'ridelibrary', component: RidelibraryComponent},
+{path: 'about', component: AboutComponent}
 ];
 
 export const routes: ModuleWithProviders = RouterModule.forRoot(router);
